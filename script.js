ad02c961d25cf95edc758f7fef833e86135754a8
GiovanniTidei <60188364+GiovanniTidei@users.noreply.github.com>
2024-04-28 14:18:49 +0200

    fix dimension locate and text
    smal fix dimesnione

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function addMarker(lat, lng, popupText, imageUrl, imageWidth, imageHeight) {
     });
 }
 
-addMarker(41.8902, 12.4922, "<b>Il Colosseo</b><br>,conosciuto anche come Anfiteatro Flavio, è il più grande anfiteatro di epoca romana. L'anfiteatro fu edificato in epoca flavia su un'area al limite orientale del Foro Romano. La sua costruzione, iniziata da Vespasiano nel 70 dopo Cristo, fu conclusa da Tito, che lo inaugurò il 21 aprile nell'80 dopo Cristo. Situato nel centro della città di Roma, vicino al foro romano, è il più grande anfiteatro mai costruito. Ha una lunghezza di 189 metri, una larghezza di 156 metri e un'altezza di 48 metri. ", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1599px-Colosseo_2020.jpg", 200, 133);
+addMarker(41.8902, 12.4922, "Il Colosseo,conosciuto anche come Anfiteatro Flavio, è il più grande anfiteatro di epoca romana. L'anfiteatro fu edificato in epoca flavia su un'area al limite orientale del Foro Romano. La sua costruzione, iniziata da Vespasiano nel 70 dopo Cristo, fu conclusa da Tito, che lo inaugurò il 21 aprile nell'80 dopo Cristo. Situato nel centro della città di Roma, vicino al foro romano, è il più grande anfiteatro mai costruito. Ha una lunghezza di 189 metri, una larghezza di 156 metri e un'altezza di 48 metri. ", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1599px-Colosseo_2020.jpg", 200, 133);
 
 function addCustomMarker(lat, lng, popupText, imageUrl, imageWidth, imageHeight) {
     addMarker(lat, lng, popupText, imageUrl, imageWidth, imageHeight);
@@ -57,7 +57,7 @@ addCustomMarker(41.885905, 12.506156,
 
 // Pantheon
 addCustomMarker(41.898609, 12.476873,
-    "Il Pantheon è uno dei monumenti più iconici di Roma. Costruito originariamente nel 27 avanti Cristo, da Agrippa, come tempio dedicato a tutti gli dei dell'Olimpo. è stato successivamente ricostruito dall'imperatore Adriano, nel 125 dopo Cristo.  È famoso per la sua cupola emisferica, che è stata un'impresa architettonica straordinaria per l'epoca, e rimane la più grande del mondo, non supportata da alcun sostegno interno. All'interno, il Pantheon ospita sepolture di personaggi illustri, tra cui il pittore Raffaello Sanzio e il re Vittorio Emanuele II. L'atmosfera solenne all'interno è arricchita dalle opere d'arte e dalle decorazioni, mentre la luce naturale che filtra attraverso l'apertura nella cupola, chiamata oculo, aggiunge un tocco mistico all'ambiente.",
+    "Il Pantheon è uno dei monumenti più iconici di Roma. Costruito originariamente nel 27 avanti Cristo, da Agrippa, come tempio dedicato a tutti gli dei dell'Olimpo. è stato successivamente ricostruito dall'imperatore Adriano, nel 125 dopo Cristo. È famoso per la sua cupola emisferica, che è stata un'impresa architettonica straordinaria per l'epoca, e rimane la più grande del mondo, non supportata da alcun sostegno interno. All'interno, il Pantheon ospita sepolture di personaggi illustri, tra cui il pittore Raffaello Sanzio e il re Vittorio Emanuele II. L'atmosfera solenne all'interno è arricchita dalle opere d'arte e dalle decorazioni, mentre la luce naturale che filtra attraverso l'apertura nella cupola, chiamata oculo, aggiunge un tocco mistico all'ambiente.",
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Pantheon_%28Rome%29_-_Right_side_and_front.jpg/520px-Pantheon_%28Rome%29_-_Right_side_and_front.jpg",
     200, 133);
 // Vittoriano
@@ -69,7 +69,7 @@ addCustomMarker(41.8946, 12.4831,
 //Piramide Cestia
 
 addCustomMarker(41.876389, 12.480833,
-    "La Piramide Cèstia  (o Piramide di Gaio Cestio) è una tomba romana, a forma di piramide. Costruita intorno al 12 avanti Cristo, fu commissionata da Gaio Cestio Epulone, un funzionario romano, come suo sepolcro. La piramide è un esempio unico di architettura egizia, all'interno di Roma antica, riflettendo l'influenza dell'Egitto sul mondo romano, in quel periodo. Costruita in travertino e marmo, la sua forma piramidale rappresentava l'esotismo e il prestigio dell'antica civiltà egizia, mentre la sua costruzione rifletteva la moda funeraria romana dell'epoca. La Piramide Cèstia rimane oggi come un'iconica testimonianza dell'interconnessione culturale nell'antichità romana. La piramide è inglobata nel perimetro del posteriore cimitero acattolico, costruito tra il diciottesimo e il diciannovesimo secolo.",
+    "La Piramide Cestia  (o Piramide di Gaio Cestio) è una tomba romana, a forma di piramide. Costruita intorno al 12 avanti Cristo, fu commissionata da Gaio Cestio Epulone, un funzionario romano. La piramide è un esempio unico di architettura egizia all'interno della Roma antica, riflette l'influenza dell'Egitto sul mondo romano in quel periodo. Costruita in travertino e marmo, la sua forma piramidale rappresentava l'esotismo e il prestigio dell'antica civiltà egizia, mentre la sua costruzione rifletteva la moda funeraria romana dell'epoca. La Piramide Cèstia rimane oggi come un'iconica testimonianza dell'interconnessione culturale nell'antichità romana. La piramide è inglobata nel perimetro del posteriore cimitero acattolico, costruito tra il diciottesimo e il diciannovesimo secolo.",
     "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Piramide_Cestia.jpg/520px-Piramide_Cestia.jpg",
     200, 133);
 
